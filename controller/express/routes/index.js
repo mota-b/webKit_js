9cf84c0f27788d44de3c0355066992ac7a753dac
mota-b <gbenhatchi@hotmail.fr>
2019-02-05 00:27:33 +0000

    init the webkit

---
+++ controller/express/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('indexView', { 
+    title: 'Express', 
+    links: [
+      "https://use.fontawesome.com/releases/v5.7.0/css/all.css", // Font Awesome
+      "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css", // Bootstrap Core
+      "https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.1/css/mdb.min.css", // Material Design Bootstrap 
+      "/css/indexStyle.css" // Custom css of the route
+    ],
+    scripts: [
+      "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js", // jQuery
+      "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js", // Bootstrap tooltips
+      "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.min.js", // Bootstrap Core
+      "https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.1/js/mdb.min.js", // Material Design Bootstrap JS 
+      "/js/indexScript.js" // Custom javascripts of the route
+    ] 
+  });
+
+});
+
+module.exports = router;
